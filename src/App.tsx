2d0d8c6e53db005620730d8dc43cc37df768a845
AlexTAHI <62557152+alexxtahi@users.noreply.github.com>
2023-02-20 12:09:20 +0100

    Template integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import HomeView from './views/HomeView';
 import './App.css';
 
 function App() {
+  return (
+    <HomeView/>
+  );
   return (
     <div className="App">
       <header className="App-header">
