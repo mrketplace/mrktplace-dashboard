40b35fd214ca581045051cc32351bce4c446a793
AlexTAHI <62557152+alexxtahi@users.noreply.github.com>
2023-03-02 20:25:15 +0100

    Update ShopCard design

--- src/components/ShopCard.tsx
+++ src/components/ShopCard.tsx
@@ -1,17 +1,19 @@
 export default function ShopCard(props: any) {
     // Component rendering
     return (
-        <div className="col-md-6 col-lg-4 mb-3">
-            <div className="card h-100">
+        <a href={props.shop.url} className="col-md-6 col-lg-4 mb-3 shop-card-link">
+            {/* <div className="col-md-6 col-lg-4 mb-3"> */}
+            <div className="card h-100 shop-card">
                 <img className="card-img-top w-100 h-100" style={{ objectFit: 'cover' }} src={props.shop.img} alt="Card image cap" />
                 <div className="card-body">
                     <h5 className="card-title">{props.shop.name}</h5>
-                    <p className="card-text">
+                    <p className="card-text shop-card-text">
                         {props.shop.description}
                     </p>
-                    <a href={props.shop.url} className="btn btn-outline-primary">Voir</a>
+                    {/* <a href={props.shop.url} className="btn btn-outline-primary">Voir</a> */}
                 </div>
             </div>
-        </div>
+            {/* </div > */}
+        </a>
     );
 }
